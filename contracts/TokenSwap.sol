38282b828c422a5e2844589c04efcf1adee6c6a8
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2024-09-19 15:52:38 +0100

    changes

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -9,9 +9,6 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract TokenSwap is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
-
-    IERC20 public token;
-
     struct Order {
         address user;
         address tokenOffered;
@@ -25,33 +22,31 @@ contract TokenSwap is Ownable, ReentrancyGuard {
     mapping(uint256 => Order) public orders;
     uint256 public orderCount;
 
-    constructor(address _token) Ownable(msg.sender) {
-        token = IERC20(_token);
-    }
+    constructor() Ownable(msg.sender) {}
 
     function createOrder(
-        uint256 amountOffered,
-        uint256 amountRequested,
-        address tokenOffered,
-        address tokenRequested
+        uint256 _amountOffered,
+        uint256 _amountRequested,
+        address _tokenOffered,
+        address _tokenRequested
     ) public {
         require(
-            amountOffered > 0 && amountRequested > 0,
+            _amountOffered > 0 && _amountRequested > 0,
             "Amounts must be greater than zero"
         );
         require(
-            tokenOffered != address(0) && tokenRequested != address(0),
+            _tokenOffered != address(0) && _tokenRequested != address(0),
             "Invalid token addresses"
         );
         require(
-            IERC20(tokenOffered).balanceOf(msg.sender) >= amountOffered,
+            IERC20(_tokenOffered).balanceOf(msg.sender) >= _amountOffered,
             "Insufficient amount"
         );
         require(
-            IERC20(tokenOffered).transferFrom(
+            IERC20(_tokenOffered).transferFrom(
                 msg.sender,
                 address(this),
-                amountOffered
+                _amountOffered
             ),
             "Transfer failed"
         );
@@ -59,74 +54,74 @@ contract TokenSwap is Ownable, ReentrancyGuard {
         orderCount++;
         orders[orderCount] = Order({
             user: msg.sender,
-            amountOffered: amountOffered,
-            amountRequested: amountRequested,
-            tokenOffered: tokenOffered,
-            tokenRequested: tokenRequested,
+            amountOffered: _amountOffered,
+            amountRequested: _amountRequested,
+            tokenOffered: _tokenOffered,
+            tokenRequested: _tokenRequested,
             timestamp: block.timestamp,
             isActive: true
         });
     }
 
-    function cancelOrder(uint256 id) public {
+    function cancelOrder(uint256 _id) public {
         require(
-            orders[id].user == msg.sender,
+            orders[_id].user == msg.sender,
             "Only the creator can cancel the order"
         );
-        require(orders[id].isActive, "Order is already cancelled");
+        require(orders[_id].isActive, "Order is already cancelled");
 
-        IERC20(orders[id].tokenOffered).transfer(
+        IERC20(orders[_id].tokenOffered).transfer(
             msg.sender,
-            orders[id].amountOffered
+            orders[_id].amountOffered
         );
-        orders[id].isActive = false;
+        orders[_id].isActive = false;
     }
 
-    function executeOrder(uint256 id) public nonReentrant {
-        require(orders[id].isActive, "Order is already cancelled");
+    function executeOrder(uint256 _id) public nonReentrant {
+        require(orders[_id].isActive, "Order is already cancelled");
         require(
-            orders[id].user != msg.sender,
+            orders[_id].user != msg.sender,
             "Only the counterparty can execute the order"
         );
         require(
-            IERC20(orders[id].tokenRequested).balanceOf(msg.sender) >=
-                orders[id].amountRequested,
+            IERC20(orders[_id].tokenRequested).balanceOf(msg.sender) >=
+                orders[_id].amountRequested,
             "You do not have sufficient balance"
         );
 
         require(
-            IERC20(orders[id].tokenRequested).transferFrom(
+            IERC20(orders[_id].tokenRequested).transferFrom(
                 msg.sender,
-                orders[id].user,
-                orders[id].amountRequested
+                orders[_id].user,
+                orders[_id].amountRequested
             ),
             "Transfer failed"
         );
 
         require(
-            IERC20(orders[id].tokenOffered).transfer(
+            IERC20(orders[_id].tokenOffered).transfer(
                 msg.sender,
-                orders[id].amountOffered
+                orders[_id].amountOffered
             ),
             "Failed to transfer token"
         );
 
-        orders[id].isActive = false;
+        orders[_id].isActive = false;
     }
 
-    function getOrder(uint256 id) public view returns (Order memory) {
-        return orders[id];
+    function getOrder(uint256 _id) public view returns (Order memory) {
+        return orders[_id];
     }
 
     function getOrderCount() public view returns (uint256) {
         return orderCount;
     }
 
-    function getTokenOffered(uint256 id) public view returns (address) {
-        return orders[id].tokenOffered;
+    function getTokenOffered(uint256 _id) public view returns (address) {
+        return orders[_id].tokenOffered;
     }
 
-    function getTokenRequested(uint256 id) public view returns (address) {
-        return orders[id].tokenRequested;
+    function getTokenRequested(uint256 _id) public view returns (address) {
+        return orders[_id].tokenRequested;
     }
 }
